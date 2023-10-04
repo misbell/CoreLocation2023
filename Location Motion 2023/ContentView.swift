d1c4e76ca2b067b6616a147c8ab06c6aefae756b
Michael Prenez-Isbell <prenezisbell@gmail.com>
2023-10-04 11:26:06 -0400

    Initial Commit

---
+++ Location Motion 2023/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Location Motion 2023
+//
+//  Created by michael isbell on 10/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Location Motion 2023/Location_Motion_2023App.swift
@@ -0,0 +1,17 @@
+//
+//  Location_Motion_2023App.swift
+//  Location Motion 2023
+//
+//  Created by michael isbell on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Location_Motion_2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
