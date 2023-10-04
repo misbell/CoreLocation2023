1ddada54223b9fc607b05bd7ccb93015ee84e818
Michael Prenez-Isbell <prenezisbell@gmail.com>
2023-10-04 13:25:57 -0400

    new changes

--- Location Motion 2023/ContentView.swift
+++ Location Motion 2023/ContentView.swift
@@ -6,15 +6,31 @@
 //
 
 import SwiftUI
+import CoreLocation
 
 struct ContentView: View {
+    @StateObject var locationDataManager = LocationDataManager()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+                    switch locationDataManager.locationManager.authorizationStatus {
+                    case .authorizedWhenInUse:  // Location services are available.
+                        // Insert code here of what should happen when Location services are authorized
+                        Text("Your current location is:")
+                        Text("Latitude: \(locationDataManager.locationManager.location?.coordinate.latitude.description ?? "Error loading")")
+                        Text("Longitude: \(locationDataManager.locationManager.location?.coordinate.longitude.description ?? "Error loading")")
+                        
+                    case .restricted, .denied:  // Location services currently unavailable.
+                        // Insert code here of what should happen when Location services are NOT authorized
+                        Text("Current location data was restricted or denied.")
+                    case .notDetermined:        // Authorization not determined yet.
+                        Text("Finding your location...")
+                        ProgressView()
+                        
+                    default:
+                        ProgressView()
+                    }
+            Text("...we're good")
+                }
         .padding()
     }
 }
