3ad4b901a4042a667a3bf8af8068012a0a578967
Michael Prenez-Isbell <prenezisbell@gmail.com>
2023-10-07 16:42:58 -0400

    ongoing

--- Location Motion 2023/ContentView.swift
+++ Location Motion 2023/ContentView.swift
@@ -8,30 +8,47 @@
 import SwiftUI
 import CoreLocation
 
+
 struct ContentView: View {
     @StateObject var locationDataManager = LocationDataManager()
     var body: some View {
         VStack {
-                    switch locationDataManager.locationManager.authorizationStatus {
-                    case .authorizedWhenInUse:  // Location services are available.
-                        // Insert code here of what should happen when Location services are authorized
-                        Text("Your current location is:")
-                        Text("Latitude: \(locationDataManager.locationManager.location?.coordinate.latitude.description ?? "Error loading")")
-                        Text("Longitude: \(locationDataManager.locationManager.location?.coordinate.longitude.description ?? "Error loading")")
-                        
-                    case .restricted, .denied:  // Location services currently unavailable.
-                        // Insert code here of what should happen when Location services are NOT authorized
-                        Text("Current location data was restricted or denied.")
-                    case .notDetermined:        // Authorization not determined yet.
-                        Text("Finding your location...")
-                        ProgressView()
-                        
-                    default:
-                        ProgressView()
-                    }
+            switch locationDataManager.locationManager!.authorizationStatus {
+            case .authorizedWhenInUse:  // Location services are available.
+                // Insert code here of what should happen when Location services are authorized
+                Text("Your current location is:")
+                Text("Latitude: \(locationDataManager.locationManager!.location?.coordinate.latitude.description ?? "Error loading")")
+                Text("Longitude: \(locationDataManager.locationManager!.location?.coordinate.longitude.description ?? "Error loading")")
+                
+            case .restricted, .denied:  // Location services currently unavailable.
+                // Insert code here of what should happen when Location services are NOT authorized
+                Text("Current location data was restricted or denied.")
+            case .notDetermined:        // Authorization not determined yet.
+                Text("Finding your location...")
+                ProgressView()
+                
+            default:
+                ProgressView()
+            }
             Text("...we're good")
-            Button
-                }
+            
+            // create buttons to start and stop updates
+            
+            Button(action: {
+                locationDataManager.locationManager!.startUpdatingLocation()
+            }, label: {
+                Text("start getting updates")
+                    .background(Color.green)
+            })
+            
+            Button(action: {
+                locationDataManager.locationManager!.stopUpdatingLocation()
+            }, label: {
+                Text("STOP getting updates")
+                    .background(Color.red)
+            })
+            
+        }
         .padding()
     }
 }
--- Location Motion 2023/LocationDataManager.swift
+++ Location Motion 2023/LocationDataManager.swift
@@ -9,22 +9,50 @@ import Foundation
 import CoreLocation
 
 class LocationDataManager : NSObject, ObservableObject, CLLocationManagerDelegate {
-    var locationManager = CLLocationManager()
+    var locationManager : CLLocationManager? = CLLocationManager()
     @Published var authorizationStatus: CLAuthorizationStatus?
     
     override init() {
         super.init()
-        locationManager.delegate = self
+        
+        // consider walking apps vs driving apps, distance resolution etc
+        
+        guard let locmanager = locationManager else { return } // actually fail and close the app
+        
+        locmanager.delegate = self
+        locmanager.activityType = .fitness // or automotive, airborne, other, // fitness, indoor positioning disabled
+        locmanager.desiredAccuracy = kCLLocationAccuracyBestForNavigation // most accurate and power intensive
+        locmanager.distanceFilter = 1 // 1 meter // standard servics only, not visits or significantchange
+        locmanager.allowsBackgroundLocationUpdates = true
+        
+        // accuracy within ten meters, hundred, kilometer, three kilometers
+        
+        if #available(iOS 14.0, *) {
+            
+            // set this on the context view
+            
+            switch locmanager.accuracyAuthorization {
+            case .fullAccuracy:
+                print("Full Accuracy")
+            case .reducedAccuracy:
+                print("Reduced Accuracy")
+            @unknown default:
+                print("Unknown Precise Location...")
+            }
+            
+        }
+        
+        
     }
     
-
+    
     
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         switch manager.authorizationStatus {
         case .authorizedWhenInUse:  // Location services are available.
             // Insert code here of what should happen when Location services are authorized
             authorizationStatus = .authorizedWhenInUse
-            locationManager.requestLocation()
+            locationManager!.requestLocation()
             break
             
         case .restricted:  // Location services currently unavailable.
@@ -70,9 +98,9 @@ class LocationDataManager : NSObject, ObservableObject, CLLocationManagerDelegat
 
 
 
-            // locationManager.startUpdatingLocation()
-            // locationManager.startMonitoringSignificantLocationChanges()
-            // locationManager.startMonitoringVisits()
-            // locationManager.startMonitoringLocationPushes()
-            // locationManager.stopUpdatingLocation()
-            // locationManager.startMonitoringLocationPushes(completion: <#T##((Data?, Error?) -> Void)?##((Data?, Error?) -> Void)?##(Data?, Error?) -> Void#>)
+// locationManager.startUpdatingLocation()
+// locationManager.startMonitoringSignificantLocationChanges()
+// locationManager.startMonitoringVisits()
+// locationManager.startMonitoringLocationPushes()
+// locationManager.stopUpdatingLocation()
+// locationManager.startMonitoringLocationPushes(completion: <#T##((Data?, Error?) -> Void)?##((Data?, Error?) -> Void)?##(Data?, Error?) -> Void#>)
